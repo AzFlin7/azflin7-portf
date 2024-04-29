6b7ccb5d66784a9b0287204700564905dab82b9f
AzFlin7 <dmytro.web2498@gmail.com>
2024-04-29 10:21:29 +0300

    update portfolio detail page

---
+++ details.js
@@ -0,0 +1,42 @@
+function getDetails(index) {
+  fetch('assets/projectDetail.json')
+  .then(response => response.json())
+  .then(data => {
+      // Access the keys and values of the JSON data
+    data.forEach(item => {
+      // const titleToFind = item.title;
+      if(item.id == index) {
+        const swiper_slide_wrapper = document.getElementsByClassName('swiper-wrapper align-items-center')[0];
+        for (let i = 0; i < item.imgs.length; i++) {
+          let swiper_slide = document.createElement('div');
+          swiper_slide.className = "swiper-slide";
+          let slide_img = document.createElement('img');
+          slide_img.src = "assets/img/portfolio/" + item.imgs[i];
+          swiper_slide.appendChild(slide_img);
+          swiper_slide_wrapper.appendChild(swiper_slide);
+        }
+        const title = document.getElementById('title');
+        title.innerHTML = item.title;
+        const category = document.getElementById('category');
+        category.innerHTML = ": " + item.category;
+        const company = document.getElementById('company');
+        company.innerHTML = ": " + item.client;
+        const projectLink = document.getElementById('link');
+        projectLink.href = item.link;
+        projectLink.innerHTML = item.link;
+        const description = document.getElementById('description');
+        description.innerHTML = item.description;
+      }
+    });
+  })
+  .catch(error => console.error('Error fetching JSON:', error));
+}
+
+$(document).ready(function(){
+  const queryString  = window.location.search;
+  const idMatch = queryString.match(/id=([^&]*)/);
+  console.log("Is match? ", idMatch);
+  let id = idMatch ? idMatch[1] : 1;
+  id > 30 ? id = 1: parseInt(id);
+  getDetails(id);
+});
---
+++ index.js
@@ -0,0 +1,40 @@
+fetch('assets/projectDetail.json')
+    .then(response => response.json())
+    .then(data => {
+        // Access the keys and values of the JSON data
+      data.forEach(item => {
+        // const titleToFind = item.title;
+        const portfolio_container = document.getElementsByClassName('portfolio-container')[0];
+        const portfolio_item = document.createElement('div');
+        portfolio_item.className = "col-lg-4 col-md-6 portfolio-item " + item.filter
+        const portfolio_wrap = document.createElement('div');
+        portfolio_wrap.className = "portfolio-wrap";
+        const imgElement = document.createElement('img');
+        imgElement.src = "assets/img/portfolio/" + item.img;
+        imgElement.className = "img-fluid";
+        const portfolio_links = document.createElement('div');
+        portfolio_links.className = "portfolio-links";
+        const shortLink = document.createElement('a');
+        shortLink.href = "assets/img/portfolio/"+item.img;
+        shortLink.setAttribute('data-gallery', "portfolioGallery");
+        shortLink.className = "portfolio-lightbox";
+        shortLink.title = item.title;
+        const shortIcon = document.createElement('i');
+        shortIcon.className = "bi bi-arrows-angle-expand";
+        shortLink.appendChild(shortIcon);
+        const detailLink = document.createElement('a');
+        detailLink.href = "portfolio-details.html?id=" + item.id;
+        detailLink.title = "More Details";
+        const detailIcon = document.createElement('i');
+        detailIcon.className = "bx bx-link";
+        detailLink.appendChild(detailIcon);
+        portfolio_links.appendChild(shortLink);
+        portfolio_links.appendChild(detailLink);
+        portfolio_wrap.appendChild(imgElement);
+        portfolio_wrap.appendChild(portfolio_links);
+        portfolio_item.appendChild(portfolio_wrap);
+
+        portfolio_container.appendChild(portfolio_item);
+      });
+    })
+    .catch(error => console.error('Error fetching JSON:', error));
